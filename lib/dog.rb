113c645bdcb12cad1e71b0649013ebe1978ff41b
Marc Steele <marcsteele2014@gmail.com>
2020-10-15 15:27:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,9 @@
 class Dog
 
-def name=(dog_name)
-  @this_dogs_name = dog_name
+def instance_variable_set(:@name)
+  
 end
 
-def name
-  @this_dogs_name
-end
-
-def breed=(dog_breed)
-  @this_dogs_breed = dog_breed
-end
-
-def breed
-  @this_dogs_breed
-end
 
 end
+
