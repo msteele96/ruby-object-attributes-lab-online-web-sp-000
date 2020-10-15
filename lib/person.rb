859f0e1db2abe2a5f94a52b4d04288c2b861aac2
Marc Steele <marcsteele2014@gmail.com>
2020-10-15 16:04:40 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(occupation)
+    @job = occupation
+  end
+
+  def job
+    @job
+  end
+
+end
